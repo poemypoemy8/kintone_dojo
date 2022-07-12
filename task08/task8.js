79bd9c77ea2505aace260772d22512b83c40c22e
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-12 19:11:14 +0900

    task08 sort

--- task08/task8.js
+++ task08/task8.js
@@ -15,14 +15,47 @@
       .then((resp) =>{
         console.log(resp);
         //task6と同じように、respから取ってきたデータをvalueの中に設定する
-  
+        let newRow = [];
+        const action5 = resp.properties.Table.fields.Action5.options;
+        let result = resp['properties']['Table']['fields']['Action5']['options'].map(function(rec) {
+          //①
+          return {
+            rec : rec
+          , key : resp['properties']['Table']['fields']['Action5']['options']['index']
+          };
+        }).sort(function(a, b){    
+          //②
+          return (a.key < b.key) ? -1 : 1;  //オブジェクトの昇順ソート
+        }).map(function(obj) {
+          //③
+          return obj.rec;
+        });
+
+        console.log(result);
+
+        // const actionArray = Object.keys(action5);
+        // console.log(actionArray); //length:6
+
+        action5.forEach((val,index) => {
+          newRow[index] = resp.properties.Table.fields.Action5[val];
+          console.log(newRow[index]);
+        });
+        // console.log(newRow);
+
+        // event.record.Table.value = newRow;
+        
+
       })
       .catch((err) =>{
         // console.log(err);
       })
+
       return event;
     });
   
 
   
   })();
+
+  // 参考URL https://keizokuma.com/js-array-object-sort/
+
