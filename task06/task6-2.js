b53d6899aaa5de4622cd172512b605ea23927a0e
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-13 14:11:32 +0900

    task06 space fixed

--- task06/task6-2.js
+++ task06/task6-2.js
@@ -1,34 +1,32 @@
 (() => {
   'use strict';
-  const action5 = ['あくなき探求','不屈の心体','理想への共感','心を動かす','知識を増やす','公明正大'];
+  const action5 = ['あくなき探求', '不屈の心体', '理想への共感', '心を動かす', '知識を増やす', '公明正大'];
   const newRow = [];
 
-  kintone.events.on('app.record.create.show',(event) =>{
+  kintone.events.on('app.record.create.show', (event) => {
 
-    action5.forEach((val,index)=>{
-      newRow[index] = 
-      {
-        'value':{
-          'Action5':{
-            'type':'DROP_DOWN',
-            'value':action5[index],
+    action5.forEach((val, index) => {
+      newRow[index] = {
+        'value': {
+          'Action5': {
+            'type': 'DROP_DOWN',
+            'value': action5[index],
           },
-          '課題':{
-            'type':'MULTI_LINE_TEXT',
-            'value':'',
+          '課題': {
+            'type': 'MULTI_LINE_TEXT',
+            'value': '',
           },
-          '状況':{
-            'type':'CHECK_BOX',
-            'value':['未振り返り']
+          '状況': {
+            'type': 'CHECK_BOX',
+            'value': ['未振り返り']
           }
         }
-      }; 
+      };
       event.record.Table.value[index] = newRow[index];
     });
 
     return event;
 
   });
- 
 
 })();
