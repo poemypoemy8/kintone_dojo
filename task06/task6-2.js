cb6cd317ccc3f7aa6ead1f4753b16485d5a075a4
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-13 10:30:03 +0900

    comment referenced

--- task06/task6-2.js
+++ task06/task6-2.js
@@ -1,108 +1,150 @@
 (() => {
-    'use strict';
+  'use strict';
+  const action5 = ['あくなき探求','不屈の心体','理想への共感','心を動かす','知識を増やす','公明正大'];
+  const newRow = [
+    // {
+    //   'value':{
+    //     'Action5':{
+    //       'type':'DROP_DOWN',
+    //       'value':'',
+    //     },
+    //     '課題':{
+    //       'type':'MULTI_LINE_TEXT',
+    //       'value':'',
+    //     },
+    //     '状況':{
+    //       'type':'CHECK_BOX',
+    //       'value':['未振り返り']
+    //     }
+    //   }
+    // },
+  ];
 
-    kintone.events.on('app.record.create.show' ,(event) =>{
-        console.log(event);
-        event.record.Table.value = [
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'あくなき探求'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          },
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'不屈の心体'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          },
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'理想への共感'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          },
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'心を動かす'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          },
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'知識を増やす'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          },
-          {
-            'value':{
-              'Action5':{
-                'type':'DROP_DOWN',
-                'value':'公明正大'
-              },
-              '課題':{
-                'type':'MULTI_LINE_TEXT',
-                'value':'',
-              },
-              '状況':{
-                'type':'CHECK_BOX',
-                'value':['未振り返り']
-              }
-            }
-          }
-        ];
-        return event;
-    });
+  action5.forEach((val,index)=>{
+    newRow = 
+    {
+      'value':{
+        'Action5':{
+          'type':'DROP_DOWN',
+          'value':action5[index],
+        },
+        '課題':{
+          'type':'MULTI_LINE_TEXT',
+          'value':'',
+        },
+        '状況':{
+          'type':'CHECK_BOX',
+          'value':['未振り返り']
+        }
+      }
+    } 
+  });
+
+
+  kintone.events.on('app.record.create.show' ,(event) =>{
+    event.record.Table.value = [
+      newRow
+
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // },
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // },
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // },
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // },
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // },
+      // {
+      //   'value':{
+      //     'Action5':{
+      //       'type':'DROP_DOWN',
+      //       'value':newRow
+      //     },
+      //     '課題':{
+      //       'type':'MULTI_LINE_TEXT',
+      //       'value':'',
+      //     },
+      //     '状況':{
+      //       'type':'CHECK_BOX',
+      //       'value':['未振り返り']
+      //     }
+      //   }
+      // }
+    ];
+
+    return event;
+  });
  
 
 })();
