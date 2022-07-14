d49ec4726e08b626eeaf17e056753b3256f47eb1
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-14 14:47:12 +0900

    event fixed

--- task07/task7.js
+++ task07/task7.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  const YYYYMMDD = dateFns.format(new Date(), 'YYYYMMDD'); // 日付のグローバル変数
+  let YYYYMMDD = dateFns.format(new Date(), 'YYYYMMDD'); // 日付のグローバル変数
   let product = ''; // 製品の値のグローバル変数
   let number = ''; // 管理番号のグローバル変数
 
@@ -12,8 +12,20 @@
     return event;
   });
 
-  kintone.events.on(['app.record.create.change.サイボウズ製品', 'app.record.edit.change.サイボウズ製品'], (event) => {
+  kintone.events.on([
+    'app.record.create.change.日付',
+    'app.record.edit.change.日付',
+    'app.record.create.change.サイボウズ製品',
+    'app.record.edit.change.サイボウズ製品',
+    'app.record.create.change.管理番号',
+    'app.record.edit.change.管理番号'
+  ], (event) => {
+
+    YYYYMMDD = dateFns.format(event.record.日付.value, 'YYYYMMDD');
     product = event.record.サイボウズ製品.value;
+    number = event.record.管理番号.value;
+
+    event.record.重複禁止項目_文字列.value = `${YYYYMMDD}-${product}-${number}`;
 
     switch (product) {
       case 'kintone':
@@ -36,12 +48,6 @@
 
     return event;
 
-  });
-
-  kintone.events.on(['app.record.create.change.管理番号', 'app.record.edit.change.管理番号'], (event) => {
-    number = event.record.管理番号.value;
-    event.record.重複禁止項目_文字列.value = `${YYYYMMDD}-${product}-${number}`;
-    return event;
   });
 
 })();
