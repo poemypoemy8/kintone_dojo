260f79a7dda3e25b22f5b1a1b743b30594fed1c5
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-15 10:00:27 +0900

    task09 query

--- task09/task9.js
+++ task09/task9.js
@@ -2,69 +2,23 @@
 
   'use strict';
 
-  let YYYYMMDD = dateFns.format(new Date(), 'YYYYMMDD'); // 日付のグローバル変数
-  let product = ''; // 製品の値のグローバル変数
-  let number = ''; // 管理番号のグローバル変数
-
-  kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
-    event.record.重複禁止項目.disabled = true;
-    event.record.重複禁止項目.value = `${YYYYMMDD}-${product}-${number}`;
-    return event;
-  });
-
-  kintone.events.on([
-    'app.record.create.change.日付',
-    'app.record.edit.change.日付',
-    'app.record.create.change.サイボウズ製品',
-    'app.record.edit.change.サイボウズ製品',
-    'app.record.create.change.管理番号',
-    'app.record.edit.change.管理番号'
-  ], (event) => {
-
-    YYYYMMDD = dateFns.format(event.record.日付.value, 'YYYYMMDD');
-    product = event.record.サイボウズ製品.value;
-    number = event.record.管理番号.value;
-
-    event.record.重複禁止項目.value = `${YYYYMMDD}-${product}-${number}`;
-
-    switch (product) {
-      case 'kintone':
-        product = 'KN';
-        event.record.重複禁止項目.value = `${YYYYMMDD}-${product}-${number}`;
-        break;
-      case 'Garoon':
-        product = 'GR';
-        event.record.重複禁止項目.value = `${YYYYMMDD}-${product}-${number}`;
-        break;
-      case 'サイボウズ Office':
-        product = 'OF';
-        event.record.重複禁止項目.value = `${YYYYMMDD}+${product}+${number}`;
-        break;
-      case 'Mailwise':
-        product = 'MW';
-        event.record.重複禁止項目.value = `${YYYYMMDD}-${product}-${number}`;
-        break;
-    }
-
-    return event;
-
-  });
-
-  kintone.events.on('app.record.create.show', (event) => {
+  kintone.events.on(['app.record.create.show', 'app.record.edit.show'],  (event) => {
 
     const params = {
-      app: event.appId,
+      app: event.appId, //kintone.app.getId(),
       fields: '重複禁止項目',
-      // query: 
+      // query: 重複禁止項目 = 
     }
 
-    kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params).then((resp) => {
+    return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params).then((resp) => {
 
+      console.log(event);
       console.log(resp);
 
-      if (resp.records[index].重複禁止項目 === event.record.重複禁止項目) {
-        const confirm = window.confirm('レコードが重複しています。このまま保存しますか？')
-        if (confirm) {
+      if (resp.records[index].重複禁止項目 === event.record.重複禁止項目.value) {
+        const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？')
+        if (recordConfirm) {
+          window
           return event;
         } else {
           return false;
