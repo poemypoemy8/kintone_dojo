ebd34fb167e44f115e9fed07860d57e8c6235661
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-15 16:50:44 +0900

    code reference fixed

--- task09/task9.js
+++ task09/task9.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'],  (event) => {
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
 
     const params = {
       app: kintone.app.getId(),
@@ -10,24 +10,24 @@
     };
 
     return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params)
-    .then((resp) => {
+      .then((resp) => {
 
-      for(let i = 0; i < resp.records.length; i++){
-        if (resp.records[[i]].重複禁止項目.value === event.record.重複禁止項目.value) {
-          const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？')
-          if (recordConfirm) {
-            window.alert('保存を実行しました');
-            break;
-          } else {
-            window.alert('保存操作をキャンセルしました');
-            return false;
+        for (let i = 0; i < resp.records.length; i++) {
+          if (resp.records[[i]].重複禁止項目.value === event.record.重複禁止項目.value) {
+            const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？');
+            if (recordConfirm) {
+              window.alert('保存を実行しました');
+              break;
+            } else {
+              window.alert('保存操作をキャンセルしました');
+              return false;
+            }
           }
         }
-      }
-      return event;
-    }).catch((err) => {
-      console.log(err);
-    })
-  })
+        return event;
+      }).catch((err) => {
+        console.log(err);
+      });
+  });
 
 })();
