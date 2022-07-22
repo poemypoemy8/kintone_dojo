d7309900d31a31eb432ce4782627a82681361b79
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-22 18:50:37 +0900

    fixed

--- task09/task9.js
+++ task09/task9.js
@@ -3,25 +3,30 @@
   'use strict';
 
   kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
-
+    console.log(event);
+    const value = event.record.重複禁止項目.value;
+    // const field = (event.type === 'app.record.create.submit') ? ['重複禁止項目'] : ['重複禁止項目', '$id'];
+    // console.log($id);
+    const query = (event.type === 'app.record.create.submit') ? `重複禁止項目 = "${value}"` : (`重複禁止項目 = "${value}"`);
+//id のやつは含まない形にしたい
     const params = {
       app: kintone.app.getId(),
-      fields: '重複禁止項目'
+      fields: ['重複禁止項目'], 
+      query: query, // 文字列
     };
+
 
     return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params)
       .then((resp) => {
+        console.log(resp); // debug
 
-        for (let i = 0; i < resp.records.length; i++) {
-          if (resp.records[[i]].重複禁止項目.value === event.record.重複禁止項目.value) {
-            const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？');
-            if (recordConfirm) {
-              window.alert('保存を実行しました');
-              break;
-            } else {
-              window.alert('保存操作をキャンセルしました');
-              return false;
-            }
+        if (resp.records.length > 0) {
+          const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？');
+          if (recordConfirm) {
+            window.alert('保存を実行しました');
+          } else {
+            window.alert('保存操作をキャンセルしました');
+            return false;
           }
         }
         return event;
@@ -31,3 +36,5 @@
   });
 
 })();
+
+// queryを絞り込みたい条件 重複しているレコードだけを持ってくる？
