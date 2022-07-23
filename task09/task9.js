cc53bb3c7bf95833cbc8263cf4dc222fdefc3944
poemy-cybozu <utami-okamoto@cybozu.co.jp>
2022-07-23 18:57:11 +0900

    task09 query fixed

--- task09/task9.js
+++ task09/task9.js
@@ -3,24 +3,24 @@
   'use strict';
 
   kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
-    console.log(event);
+    console.log(event); // debug
     const value = event.record.重複禁止項目.value;
-    // const field = (event.type === 'app.record.create.submit') ? ['重複禁止項目'] : ['重複禁止項目', '$id'];
-    // console.log($id);
-    const query = (event.type === 'app.record.create.submit') ? `重複禁止項目 = "${value}"` : (`重複禁止項目 = "${value}"`);
-//id のやつは含まない形にしたい
+    const field =  ['重複禁止項目'];
+    const id = kintone.app.getId();
+    console.log(id);
+    const query = (event.type === 'app.record.create.submit') ? `重複禁止項目 = "${value}"` : `重複禁止項目 = "${value}" and $id != "${id}"`;
     const params = {
-      app: kintone.app.getId(),
-      fields: ['重複禁止項目'], 
-      query: query, // 文字列
+      app: id,
+      fields: field,
+      query: query,
+      totalCount : true
     };
-
 
     return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', params)
       .then((resp) => {
         console.log(resp); // debug
 
-        if (resp.records.length > 0) {
+        if (resp.totalCount > 0) {
           const recordConfirm = window.confirm('レコードが重複しています。このまま保存しますか？');
           if (recordConfirm) {
             window.alert('保存を実行しました');
